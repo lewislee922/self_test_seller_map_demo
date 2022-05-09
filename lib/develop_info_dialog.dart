d3f228e76a65b2aa56824eba73bb400de15090a0
lewislee922 <p81922@gmail.com>
2022-05-09 20:52:55 +0800

    UI fix

--- lib/develop_info_dialog.dart
+++ lib/develop_info_dialog.dart
@@ -6,8 +6,10 @@ class DeveloperInfoDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
+      contentPadding: EdgeInsets.all(8.0),
       children: [
         Text("利用衛福部Open Data製作之快篩販售地圖\n可快速查詢線上剩餘庫存、電話，並開啟手機地圖App導航"),
+        SizedBox(height: 16.0),
         Text("版本：1.0.0+1")
       ],
     );
